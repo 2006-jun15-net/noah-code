dd0fded1df0c15854f14be5451da7285734c3d17
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-07-02 14:43:36 -0600

    MVC notes, examples, and solution files

---
+++ HelloWorldWebApp/HelloWorldWebApp/HelloWorldWebApp/Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldWebApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Hello1()
+        {
+            //var result = new ContentResult
+            //{
+            //    StatusCode = 200, //default anyway
+            //    ContentType = "test/html",
+            //    Content = "<html><head></head><body>Hello controller</body></html>"
+
+            //};
+            //return result;
+            ViewResult result = View("Hello", "this string is the model");
+            return result;
+        }
+
+
+        //this class can contain action methods and nonaction methods
+        [NonAction]
+        public void HelperMethod()
+        {
+
+        }
+    }
+}
--- HelloWorldWebApp/HelloWorldWebApp/HelloWorldWebApp/Startup.cs
+++ HelloWorldWebApp/HelloWorldWebApp/HelloWorldWebApp/Startup.cs
@@ -11,24 +11,36 @@ using Microsoft.Extensions.Hosting;
 
 namespace HelloWorldWebApp
 {
+    // a controller will take responsibility for ahndling some subset of requests to the app
+    // usually based upon the path in the URL
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //teach asp.net about controllers
+            //services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            //app.UseRouting();
+            app.UseRouting();
 
+            app.UseEndpoints(endpoints =>
+            {
+                //C# supports anonymous types like new {prop = "a", prop2 = 1}
+                // we can set up multiple routes - each one will give a 
+                endpoints.MapControllerRoute("Hello-route", "hello", new { controller = "Hello", action = "Hello1" } );
+                endpoints.MapControllerRoute("default", "{controller}/{action}");
+            });
             //app.UseEndpoints(endpoints =>
             //{
             //    endpoints.MapGet("/", async context =>
@@ -44,6 +56,7 @@ namespace HelloWorldWebApp
             //    context.Response.ContentType = "text/html";
             //    await context.Response.WriteAsync("<!DOCTYPE html> <html><head></head><body>Hello world</bod></html>");
             //});
+
             // for every request, loook in the url for a relative path, and repsond
             // with the contents of that file
             app.Run(async context =>
