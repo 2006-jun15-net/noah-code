8898105dd095fbc850a385eadd59546bdd245acf
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-07-14 15:01:38 -0600

    KitchenService SOAP example and notes

---
+++ Week5/KitchenService/KitchenService/Fridge.svc.cs
@@ -0,0 +1,33 @@
+﻿using KitchenService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace KitchenService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Fridge" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Fridge.svc or Fridge.svc.cs at the Solution Explorer and start debugging.
+    public class Fridge : IFridge
+    {
+        private static List<FoodItem> _contents = new List<FoodItem>
+        {
+            new FoodItem{Id = 1, Name = "expired cheese", ExpirationDate = new DateTime(2020, 6, 14)},
+            new FoodItem{Id = 2, Name = "steak", ExpirationDate = new DateTime(2020, 7, 20)}
+        };
+        public ICollection<FoodItem> Clean()
+        {
+            var removed = _contents.Where(i => i.ExpirationDate < DateTime.Now).ToHashSet();
+            _contents = _contents.Except(removed).ToList();
+            return removed;
+        }
+
+        public ICollection<FoodItem> GetAllItems()
+        {
+            
+            return _contents.ToHashSet();
+        }
+    }
+}
---
+++ Week5/KitchenService/KitchenService/IFridge.cs
@@ -0,0 +1,22 @@
+﻿using KitchenService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace KitchenService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFridge" in both code and config file together.
+    [ServiceContract]
+    public interface IFridge
+    {
+        [OperationContract]
+        ICollection<FoodItem> GetAllItems();
+
+        //remove all the expired items and return them
+        [OperationContract]
+        ICollection<FoodItem> Clean();
+    }
+}
