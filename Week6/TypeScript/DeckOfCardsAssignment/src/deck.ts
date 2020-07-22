4d9eadc2cd99324c7b916a974db7a37bb7696ba3
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-07-21 20:24:22 -0600

    deckofcards complete

--- Week6/TypeScript/DeckOfCardsAssignment/src/deck.ts
+++ Week6/TypeScript/DeckOfCardsAssignment/src/deck.ts
@@ -1,5 +1,5 @@
 export default interface Deck{
-    DeckId: number;
+    DeckId: string;
     shuffled: boolean;
     remaining: number;
 
--- Week6/TypeScript/DeckOfCardsAssignment/src/index.ts
+++ Week6/TypeScript/DeckOfCardsAssignment/src/index.ts
@@ -1,46 +1,50 @@
-import Deck from "./deck";
-import Card from "./card";
+//import Deck from "./deck";
+//import Card from "./card";
 
-GetNewDeck(){
-    fetch("https://deckofcardsapi.com/api/deck/new/",
+const drawButton = document.getElementById("draw-button");
+
+if(drawButton)
+{
+    drawButton.addEventListener("click", DrawCard);
+}
+
+
+// function GetNewDeck(){
+//     fetch("https://deckofcardsapi.com/api/deck/new/",
+//     {
+//         method: "get",
+//         headers: {"Accept": "application/json"}
+//     })
+//     .then(response => response.json())
+//     .then(deck => {
+//         if(deck.success === true)
+//         {
+//             let newDeck: Deck = {
+//                 DeckId: deck.deck_id,
+//                 shuffled: deck.shuffled,
+//                 remaining: deck.remaining
+//             }
+//             const d = document.createElement("p");
+//             d.innerHTML = `${newDeck.DeckId} ${newDeck.remaining}`;
+//             document.body.append(d); 
+//         }
+//     })
+// }
+
+function DrawCard(){
+    
+    fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=1",
     {
         method: "get",
         headers: {"Accept": "application/json"}
     })
     .then(response => response.json())
     .then(deck => {
-        if(deck.success === true)
-        {
-            let newDeck: Deck = {
-                DeckId: deck.deck_id,
-                shuffled: deck.shuffled,
-                remaining: deck.remaining
-            }
-            const d = document.createElement("p");
-            d.innerHTML = `${newDeck.DeckId} ${newDeck.remaining}`;
-            document.body.append(d); 
-        }
+        debugger;
+        const c = document.createElement("img");
+        c.src = deck.cards[0].image;
+        document.body.appendChild(c); 
     })
-}
-
-DrawCard(){
-    fetch("https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=1",
-    {
-        method: "get",
-        headers: {"Accept": "application/json"}
-    })
-    .then(response => response.json())
-    .then(card =>
-        {
-            if(card.success === true)
-            {
-                const newCard: Card = {
-                    image: card.image,
-                    value: card.value,
-                    suit: card.suit, 
-                    code: card.code
-                }
-            }
-        })
+       
 }
 
